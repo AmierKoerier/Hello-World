ec4a5c5703a50fec37f5b04fc2bd81d87ad20d0b
Matthias Hartman <matthias.hartman@hotmail.com>
2018-12-29 12:58:49 +0100

    adding gui

--- python/GUI_test.py
+++ python/GUI_test.py
@@ -1,12 +1,14 @@
 from tkinter import * 
 
+## Main:
 window = Tk()
 window.title("GUI Test")
+window.configure(width = 500, height = 500)
+
+## Label:
+## Label (window, text = "This is a test").grid(row = 0, column = 0, sticky = W)
 
 
 
-
-
-
-
+## Running the main loop: 
 window.mainloop()
